722c4ef9e362ee69f4dfbc2d985398b83008e09e
zzmudzio <zzmudzio@gmail.com>
2022-11-15 11:14:44 +0100

    added new xpath locator

--- src/main/java/pl/zzmudzio/jenkinspages/JenkinsMainPage.java
+++ src/main/java/pl/zzmudzio/jenkinspages/JenkinsMainPage.java
@@ -1,5 +1,9 @@
 package pl.zzmudzio.jenkinspages;
 
+import org.openqa.selenium.By;
+
 public class JenkinsMainPage {
+    private final static By desiredAppCategoryLocator = new By.ByXPath(".//div[@id='projectstatus-tabBar']" +
+            "/div/div/div[5]/a[text()='SAWA']");
 
 }
